7da6dbea128ba4155ad6009f4a8ff5615fbe6a38
NCM <nmitchell34@gatech.edu>
2020-08-03 14:10:37 -0400

    added table

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <h1>Here is my Header for this directory</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Salary</th>
+                  <th scope="col">Age</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Larry</td>
+                  <td>the Bird</td>
+                  <td>@twitter</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
